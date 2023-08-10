0f9f95e9ba6b26d20bb89f2bb4e5fef00f052cd4
juju0111 <p3549823@gmail.com>
2023-08-10 17:16:57 +0900

    shell script & bench 0,1,2 mcts test code

--- scripts/run_benchmark1_rearr.sh
+++ scripts/run_benchmark1_rearr.sh
@@ -1,7 +1,11 @@
-# for i in 2 3 4 5 6
-# do
-#     python3 -m examples.doosan.mcts.benchmark1_rearr_test --budgets 100 --max_depth 16 --algo random --debug_mode false --box_number 6 --seed $i --consider_next_scene 1 --use_pick_action 0  &
 
-#     sleep 3
-# done
-python3 -m examples.doosan.mcts.benchmark1_rearr_test --budgets 100 --max_depth 8 --algo random --debug_mode false --box_number 6 --seed 10 --consider_next_scene 1 --use_pick_action 0
+seed=$1
+max_depth=$2
+use_pick_action=$3
+consider_next_scene=$4
+
+python3 -m examples.doosan.mcts.benchmark1_rearr_test --budgets 100 --max_depth ${max_depth} --debug_mode false --box_number 6 --seed ${seed} --consider_next_scene ${consider_next_scene} --use_pick_action ${use_pick_action} --algo bai_perturb
+
+python3 -m examples.doosan.mcts.benchmark1_rearr_test --budgets 100 --max_depth ${max_depth} --debug_mode false --box_number 6 --seed ${seed} --consider_next_scene ${consider_next_scene} --use_pick_action ${use_pick_action} --algo bai_ucb
+
+python3 -m examples.doosan.mcts.benchmark1_rearr_test --budgets 100 --max_depth ${max_depth} --debug_mode false --box_number 6 --seed ${seed} --consider_next_scene ${consider_next_scene} --use_pick_action ${use_pick_action} --algo uct
---
+++ scripts/run_benchmark3_rearr.sh
@@ -0,0 +1,12 @@
+
+
+seed=$1
+max_depth=$2
+use_pick_action=$3
+consider_next_scene=$4
+
+python3 -m examples.doosan.mcts.benchmark3_rearr_test --budgets 100 --max_depth ${max_depth} --debug_mode false --box_number 6 --seed ${seed} --consider_next_scene ${consider_next_scene} --use_pick_action ${use_pick_action} --algo bai_perturb
+
+python3 -m examples.doosan.mcts.benchmark3_rearr_test --budgets 100 --max_depth ${max_depth} --debug_mode false --box_number 6 --seed ${seed} --consider_next_scene ${consider_next_scene} --use_pick_action ${use_pick_action} --algo bai_ucb
+
+python3 -m examples.doosan.mcts.benchmark3_rearr_test --budgets 100 --max_depth ${max_depth} --debug_mode false --box_number 6 --seed ${seed} --consider_next_scene ${consider_next_scene} --use_pick_action ${use_pick_action} --algo uct
